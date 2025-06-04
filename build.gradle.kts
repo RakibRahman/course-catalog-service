b166855fa8ce16f15c8ea2502dcb0d7ba89c1930
RakibRahman <md.rr.talukder@gmail.com>
2025-06-04 21:19:32 +0600

    implement logger

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	//logging
+	implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
 }
 
 kotlin {
--- src/main/kotlin/com/kotlinspring/course_catalog_service/controller/GreetingController.kt
+++ src/main/kotlin/com/kotlinspring/course_catalog_service/controller/GreetingController.kt
@@ -1,6 +1,9 @@
 package com.kotlinspring.course_catalog_service.controller
 
 import com.kotlinspring.course_catalog_service.services.GreetingService
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -16,10 +19,13 @@ class GreetingController (
 
     data class GreetingResponse(val message: String)
 
+    private val logger = KotlinLogging.logger {}
+
     @GetMapping("/{name}")
     fun receiveGreeting(@PathVariable("name") name: String): ResponseEntity<GreetingResponse> {
         return try {
             val payload = greetingService.getGreeting(name )
+            logger.info { "hello $name from logger" }
             ResponseEntity.ok(GreetingResponse(payload))
         }catch (ex: IllegalArgumentException){
             ResponseEntity.badRequest().body(GreetingResponse(ex.message?:"Invalid Input"))
